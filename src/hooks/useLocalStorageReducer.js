b2ef73b3c47e63403ba63d62d7fc846b70e8844b
Gabriel Mania <mania.gabb@gmail.com>
2022-11-29 12:27:43 +0800

    first commit

--- src/hooks/useLocalStorageReducer.js
+++ src/hooks/useLocalStorageReducer.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
-function useLocalStorageState(key, defaultVal) {
-  const [state, setState] = useState(() => {
+import { useEffect, useReducer } from "react";
+
+function useLocalStorageReducer(key, defaultVal, reducer) {
+  const [state, dispatch] = useReducer(reducer, defaultVal, () => {
     let value;
     try {
       value = JSON.parse(
@@ -9,12 +10,14 @@ function useLocalStorageState(key, defaultVal) {
     } catch (e) {
       value = defaultVal;
     }
-    console.log("USING", value);
     return value;
   });
+
   useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(state));
   }, [state]);
-  return [state, setState];
+
+  return [state, dispatch];
 }
-export { useLocalStorageState };
+
+export { useLocalStorageReducer };
